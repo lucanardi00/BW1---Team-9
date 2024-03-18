83f52b5dfa51a7da05172c8fc48a06ec4da0ac85
luca <lucabean@live.it>
2024-03-18 15:30:54 +0100

    Edited welcome.css, welcome.html and welcomejs.js

---
+++ welcomejs.js
@@ -0,0 +1,30 @@
+const checkbox = document.getElementById('checkbox1')
+
+const proceedBtn = document.querySelector('button')
+
+console.log(checkbox)
+console.log(proceedBtn)
+
+checkbox.addEventListener('click', function () {
+  if (checkbox.checked) {
+    proceedBtn.style.backgroundColor = '#00ffff'
+  } else {
+    proceedBtn.style.backgroundColor = '#e7e7eb'
+  }
+})
+
+checkbox.addEventListener('click', function () {
+  if (checkbox.checked) {
+    proceedBtn.style.backgroundColor = '#00ffff'
+  } else {
+    proceedBtn.style.backgroundColor = '#e7e7eb'
+  }
+})
+
+const proceedUrl = function () {
+  if (checkbox.checked) {
+    window.location.href = 'benchmark.html'
+  } else {
+    alert('You must tick the agreements first!')
+  }
+}
